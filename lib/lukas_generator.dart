a85f36c65435d745f8d9ac04bd9f8a29e68b0765
Kevin Lopez <rgkevin.lopez@gmail.com>
2021-03-02 15:47:40 -0600

    initial commit

---
+++ lib/lukas_generator.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library lukas_generator;
+
+export 'src/lukas_generator_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
---
+++ test/lukas_generator_test.dart
@@ -0,0 +1,16 @@
+import 'package:lukas_generator/lukas_generator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
